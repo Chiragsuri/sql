dd909626e9139b725863bab533b5a29da84703df
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2026-02-20 23:21:27 +0530

    Time: 404 ms (16.15%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -2,7 +2,7 @@
 select 
     query_name
     ,ROUND(AVG(rating * 1.0 / position), 2) as quality
-    ,ROUND(AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100, 2) as poor_query_percentage
+    ,ROUND(AVG(rating < 3) * 100, 2) as poor_query_percentage
 FROM Queries
 GROUP BY query_name
 ;

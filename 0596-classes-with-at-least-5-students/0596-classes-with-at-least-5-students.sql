e85a758a2455720dafd2ddad00e691177a3e9610
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2026-03-02 20:43:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -1,6 +1,12 @@
 # Write your MySQL query statement below
-select a.class from Courses a
-JOIN (Select class, count(student) as count from Courses
-group by class) s
-ON a.class = s.class and s.count >= 5
-Group by class
+-- select a.class from Courses a
+-- JOIN (Select class, count(student) as count from Courses
+-- group by class) s
+-- ON a.class = s.class and s.count >= 5
+-- Group by class
+
+-- With Having function
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) >= 5;
